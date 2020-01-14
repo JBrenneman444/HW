fef51081a095d1377da373245207bf1c1d0756e4
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-01-14 14:11:45 -0800

    created Person class

--- current_week_homework_assignment_name/HW-w05/donut_adventure/objects.js
+++ current_week_homework_assignment_name/HW-w05/donut_adventure/objects.js
@@ -15,3 +15,48 @@ class Hamster {
         return this.price
     }
 }
+
+class Person {
+    constructor(name) {
+        this.name = "";
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age
+    }
+    getWeight() {
+        return this.weight
+    }
+    greet() {
+        console.log(`Hello there ${Person.name}!`)
+    }
+    eat() {
+        this.weight += 1;
+        this.mood += 1;
+    }
+    exercise() {
+        this.weight += 1;
+    }
+    ageUp() {
+        this.age += 1;
+        this.height += 2;
+        this.weight += 5;
+        this.mood -= 3;
+        this.bankAccount += 10;
+    }
+    buyHamster(hamster) {
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= Hamster.getPrice();
+    }
+}
+
