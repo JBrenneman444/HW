6272ce318a714109afacc7c9ebcb4195529c1bcd
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-01-14 14:48:58 -0800

    created timmys story

--- current_week_homework_assignment_name/HW-w05/donut_adventure/objects.js
+++ current_week_homework_assignment_name/HW-w05/donut_adventure/objects.js
@@ -1,8 +1,8 @@
 
 class Hamster {
-    constructor(name) {
+    constructor(hamsterName) {
         this.owner = "";
-        this.name = "";
+        this.name = hamsterName;
         this.price = 15;
     }
     wheelRun() {
@@ -44,7 +44,7 @@ class Person {
         this.mood += 1;
     }
     exercise() {
-        this.weight += 1;
+        this.weight -= 1;
     }
     ageUp() {
         this.age += 1;
@@ -56,7 +56,63 @@ class Person {
     buyHamster(hamster) {
         this.hamsters.push(hamster);
         this.mood += 10;
-        this.bankAccount -= Hamster.getPrice();
+        this.bankAccount -= hamster.getPrice();
     }
 }
 
+let person1 = new Person("Timmy")
+
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+console.log(person1)
+
+// Have him eat five times.
+person1.eat();
+person1.eat();
+person1.eat();
+person1.eat();
+person1.eat();
+
+// Have him exercise five times
+person1.exercise();
+person1.exercise();
+person1.exercise();
+person1.exercise();
+person1.exercise();
+
+// Age Timmy 9 years -- NOTE: wasn't sure if this meant TO 9 years old or 9 additional years..
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+console.log(person1)
+
+// Create a hamster named "Gus"
+let hamster1 = new Hamster("Gus");
+console.log(hamster1)
+
+// Set Gus's owner to the string "Timmy"
+hamster1.owner = "Timmy"
+
+// Have Timmy "buy" Gus
+person1.buyHamster(hamster1);
+
+// Age Timmy 15 years
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+person1.ageUp()
+
+// Have Timmy eat twice
+person1.eat();
+person1.eat();
+
+// Have Timmy exercise twice
+person1.exercise();
+person1.exercise();
+console.log(person1)
