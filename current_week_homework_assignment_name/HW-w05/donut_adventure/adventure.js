97220d3f9c4b7425ef24cd4290908b16b11473aa
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-01-14 20:41:19 -0800

    dougie and pizza rat have fought

--- current_week_homework_assignment_name/HW-w05/donut_adventure/adventure.js
+++ current_week_homework_assignment_name/HW-w05/donut_adventure/adventure.js
@@ -12,22 +12,25 @@ class Hero {
         ];
     }    
     
-    talkSass() { // talkSass: logs one of his catchphrases randomly
-        // TODO - check math
-        Math.floor(Math.random()+2)
+    talkSass() {
+        console.log(this.catchPhrases[Math.floor(Math.random() * 2)]);
     }
 
     announceHealth() {
         console.log(`I am at ${this.health} health!`);
     }
 
-    fight() {
-        console.log('i\'m ready to rumble')
+    fight(enemy) {
+        enemy.health -= this.weapons.sugarShock;
+        console.log(`${enemy.name} just got hit by sugarShock! His health is now at ${enemy.health}`);
     }
 }
 
+// Using the hitpoints from the weapon they're using, subtract that amount from the enemy's health
+
 let hero1 = new Hero("Dougie the Donut")
 console.log(hero1)
+hero1.talkSass()
 
 class Enemy {
     constructor(enemyName){
@@ -42,11 +45,23 @@ class Enemy {
             'i\'m more dangerous than an uncovered sewer'
         ]
     }
-    talkSmack() // random catchphrase
-    announceHealth() {
-        console.log(`Im at ${this.health} health.`);
+    talkSmack() {
+        console.log(this.catchPhrases[Math.floor(Math.random() * 2)]);
     }
-    fight() {
-        console.log('i\'m gonna flatten you like a slice of pepperoni')
+    announceHealth() {
+        console.log(`I'm at ${this.health} health.`);
+    }
+    fight(enemy) {
+        enemy.health -= this.weapons.cheeseGrease;
+        console.log(`${enemy.name} just got hit by cheeseGrease! His health is now at ${enemy.health}`);
     }
 }
+
+let enemy1 = new Enemy("Pizza Rat")
+enemy1.talkSmack()
+
+hero1.announceHealth()
+enemy1.announceHealth()
+
+hero1.fight(enemy1);
+enemy1.fight(hero1);
