c50fbaed3e7de8b81b03622165da55e6d420e226
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-01-14 20:00:36 -0800

    created enemy pizza rat

--- current_week_homework_assignment_name/HW-w05/donut_adventure/adventure.js
+++ current_week_homework_assignment_name/HW-w05/donut_adventure/adventure.js
@@ -30,5 +30,23 @@ let hero1 = new Hero("Dougie the Donut")
 console.log(hero1)
 
 class Enemy {
-
+    constructor(enemyName){
+        this.name = enemyName;
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        };
+        this.catchPhrases = [
+            'i\'m youtube famous',
+            'i\'m more dangerous than an uncovered sewer'
+        ]
+    }
+    talkSmack() // random catchphrase
+    announceHealth() {
+        console.log(`Im at ${this.health} health.`);
+    }
+    fight() {
+        console.log('i\'m gonna flatten you like a slice of pepperoni')
+    }
 }
