707dd8c3f8e854d6fc46ec413ae5103f703c71d6
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-01-19 13:24:04 -0800

    commit 3

--- current_week_homework_assignment_name/HW-w06/Hogwarts/main.js
+++ current_week_homework_assignment_name/HW-w06/Hogwarts/main.js
@@ -22,6 +22,40 @@ let $h4Wand = $('<h4>')
 $h4Wand.text('Blackthorn Wand')
 $container.append($h4Wand)
 
+let $trunk = $('<ul>')
+$trunk.attr('storage','trunk')
+
+let $trunkItem1 = $('<li>')
+$trunkItem1.text('butter beer')
+$trunk.append($trunkItem1)
+
+let $trunkItem2 = $('<li>')
+$trunkItem2.text('invisibility cloak')
+$trunkItem2.addClass('secret')
+$trunk.append($trunkItem2)
+
+let $trunkItem3 = $('<li>')
+$trunkItem3.text('magic map')
+$trunkItem3.addClass('secret')
+$trunk.append($trunkItem3)
+
+let $trunkItem4 = $('<li>')
+$trunkItem4.text('time turner')
+$trunkItem4.addClass('secret')
+$trunk.append($trunkItem4)
+
+let $trunkItem5 = $('<li>')
+$trunkItem5.text('leash')
+$trunk.append($trunkItem5)
+
+let $trunkItem6 = $('<li>')
+$trunkItem6.text('Bertie Bott\'s Every Flavor [Jelly] Beans')
+$trunk.append($trunkItem6)
+
+$container.append($trunk)
+
+
+
 
 
 })
