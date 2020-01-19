00e210cea2889db47f9697c2360e9079510d971b
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-01-19 13:05:39 -0800

    commit 2

--- current_week_homework_assignment_name/HW-w06/Hogwarts/main.js
+++ current_week_homework_assignment_name/HW-w06/Hogwarts/main.js
@@ -4,4 +4,24 @@ let $container = $('#container')
 let $h1 = $('<h1>')
 $h1.text('Filler text')
 $container.append($h1)
+
+let $h2 = $('<h2>')
+$h2.text('Jake Brenneman')
+$container.append($h2)
+
+let $h3 = $('<h3>')
+$h3.text('Slytherin')
+$container.append($h3)
+
+let $h4Pet = $('<h4>')
+$h4Pet.text('Plop')
+$h4Pet.addClass('toad')
+$container.append($h4Pet)
+
+let $h4Wand = $('<h4>')
+$h4Wand.text('Blackthorn Wand')
+$container.append($h4Wand)
+
+
+
 })
