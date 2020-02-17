676f7b3ba5fc479503af37119fd59b5c86450dbe
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-02-16 16:20:01 -0800

    set up Pokemon hw files

---
+++ current_week_homework_assignment_name/W10/pokemon_app/server.js
@@ -0,0 +1,3 @@
+// The User Stories
+// When a user goes to the /pokemon route they will see an index of pokemon: the names of each pokemon rendered to the page.
+// When a user clicks on the name of the pokemon, they will be taken to that pokemon's show page, and will see the pokemon's name and image.
