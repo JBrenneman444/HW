32f2e90ad661682e5ce62c3508ddaa67a138db4c
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-02-16 16:50:30 -0800

    My server is set up and running

--- current_week_homework_assignment_name/W10/pokemon_app/server.js
+++ current_week_homework_assignment_name/W10/pokemon_app/server.js
@@ -1,3 +1,35 @@
-// The User Stories
-// When a user goes to the /pokemon route they will see an index of pokemon: the names of each pokemon rendered to the page.
-// When a user clicks on the name of the pokemon, they will be taken to that pokemon's show page, and will see the pokemon's name and image.
+//  User Stories
+    // When user clicks on the name, they will be taken to that pokemon's SHOW page, and will see the pokemon's name and image.
+
+//=================================================
+// PREREQUISITES ==================================
+//=================================================
+const express = require('express')
+const app = express()
+const port = 3000
+
+//=================================================
+// EVERYTHING ELSE ================================
+//=================================================
+
+
+//=================================================
+// ROUTES (MOST specific to LEAST) ================
+//=================================================
+app.get('/pokemon',(req,res)=>{
+    res.render('index.ejs')
+})
+
+// When user goes to /pokemon route they see index of pokemon - names of each pokemon
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome to the Pokemon App!</h1><a href="/pokemon">Go to Pokedex</a>')
+})
+
+//=================================================
+// LISTENERS ======================================
+//=================================================
+app.listen(3000, ()=>{
+    console.log('Server is LISTENING on port 3000')
+})
