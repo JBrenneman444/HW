b8df0d79d6dd55fcbed0609203fd036060247790
LocalPlacesWith <jbrenneman444@gmail.com>
2020-02-17 17:12:40 -0800

    took a break, starting again

--- current_week_homework_assignment_name/W10/budgtr/models/budget.js
+++ current_week_homework_assignment_name/W10/budgtr/models/budget.js
@@ -1,68 +1,68 @@
 module.exports = [
-    {
+    { // expenditure 1 / aka 0
       date: "April 1",
       name: "Income",
       from: "Old Glory Insurance",
       amount: 1000,
       tags: ["income", "yay"]
     },
-    {
+    { // expenditure 2
       date: "April 1",
       name: "Taxes",
       from: "Government",
       amount: -300,
       tags: ["taxes"]
     },
-    {
+    { // expenditure 3
       date: "April 1",
       name: "Retirement",
       from: "Country Bank",
       amount: -200,
       tags: ["retirement", "investing in the future"]
     },
-    {
+    { // expenditure 4
       date: "April 1",
       name: "Savings",
       from: "Country Bank",
       amount: -100,
       tags: ["savings", "rainy day fund"]
     },
-    {
+    { // expenditure 5
       date: "April 1",
       name: "Credit Card Payment",
       from: "NPM Express",
       amount: -100,
       tags: ["credit card"]
     },
-    {
+    { // expenditure 6
       date: "April 5",
       name: "Monthy Birthday Money from Aunt Tilda",
       from: "Aunt Tilda",
       amount: 20,
       tags: ["Aunt Tilda is the best"]
     },
-    {
+    { // expenditure 7
       date: "April 5",
       name: "Coffee",
       from: "Moon Coin",
       amount: -4,
       tags: ["coffee"]
     },
-    {
+    { // expenditure 8
       date: "April 5",
       name: "Internet",
       from: "Horizon",
       amount: -100,
       tags: ["utilities"]
     },
-    {
+    { // expenditure 9
       date: "April 3",
       name: "Groceries",
       from: "Merchant Jack's",
       amount: -76,
       tags: ["groceries"]
     },
-    {
+    { // expenditure 10
       date: "April 3",
       name: "Pet Food",
       from: "Pet Precious Inc",
--- current_week_homework_assignment_name/W10/budgtr/server.js
+++ current_week_homework_assignment_name/W10/budgtr/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const budgetData = require('./models/budget.js') // This is our 'database'.
+
 // see a list of your income and expenditures
 // show you one income/expenditure item
 // create a new income/expenditure item
@@ -15,7 +17,7 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/budgets', (req,res)=>{
-    res.render('index.ejs')
+    res.render('index.ejs',{budget: budgetData})
 })
 
 app.get('/budgets/new', (req,res)=>{
