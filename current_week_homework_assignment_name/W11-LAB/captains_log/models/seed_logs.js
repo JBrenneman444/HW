ae17abf5e31702f0110d53314aa4d843a7334c86
LocalPlacesWith <jbrenneman444@gmail.com>
2020-02-29 09:42:32 -0800

    finished Index Route on 2.29

---
+++ current_week_homework_assignment_name/W11-LAB/captains_log/models/seed_logs.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        title: "Captain Blackbeard",
+        entry: "Today I took a longgg dump.",
+        shipIsBroken: true
+    },
+    {
+        title: "Captain Yellow",
+        entry: "Today I took a longgg dump.",
+        shipIsBroken: true
+    },
+    {
+        title: "Captain Green",
+        entry: "Today I took a longgg dump.",
+        shipIsBroken: true
+    }
+  ]
+  
--- current_week_homework_assignment_name/W11-LAB/captains_log/server.js
+++ current_week_homework_assignment_name/W11-LAB/captains_log/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000
 
-// Connect MONGOOSE to MongoDB
+// DEPENDENCIES + Connects MONGOOSE to MongoDB
 const mongoose = require('mongoose');
+const seedData = require('./models/seed_logs.js');
+const Logs = require('./models/logs.js');
 
 //... and then farther down the file
 mongoose.connect('mongodb://localhost:27017/captainsLogDB', { useNewUrlParser: true});
@@ -14,6 +16,22 @@ mongoose.connection.once('open', ()=> {
 // MIDDLEWARE
 app.use(express.urlencoded({extended:true}));
 
+const Log = require('./models/logs.js');
+
+
+// INDEX
+app.get('/', (req, res)=>{
+    res.redirect('/logs');
+})
+
+app.get('/logs', (req, res)=>{
+    Log.find({}, (error, allLogs)=>{
+        res.render('index.ejs', {
+            logs: allLogs
+        });
+    });
+});
+
 // CREATE / POST
 app.post('/logs',(req,res)=>{
     if (req.body.shipIsBroken === 'on') { //if checked, req.body.shipIsBroken is set to 'on'
@@ -21,8 +39,10 @@ app.post('/logs',(req,res)=>{
     } else { //if not checked, req.body.shipIsBroken is undefined
         req.body.shipIsBroken = false;
     }
-    res.send(req.body)
-    console.log(req.body)
+    Log.create(req.body, (error, createdLog)=>{
+        console.log(createdLog);
+        res.redirect('/logs');
+    });
 });
 
 // NEW
